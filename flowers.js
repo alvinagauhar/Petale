86614fed8b5d50698c62f328fdbd97eb22e1b33b
rakhi <rakhiranjan16@gmail.com>
2026-09-21 01:10:50 +0530

    adding multiple flowers
    you can now add multiple flowers, cant delete tho sob
    (used ai in this part of the project)

--- flowers.js
+++ flowers.js
@@ -1,3 +1,5 @@
+let copyFlower = 0;
+
 function dragstartHandler(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
@@ -9,5 +11,23 @@ function dragoverHandler(ev) {
 function dropHandler(ev) {
     ev.preventDefault();
     const data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    const original = document.getElementById(data);
+    if (!original) return;
+
+    const box = ev.currentTarget;
+    const rect = box.getBoundingClientRect();
+
+    let item;
+    if (original.parentElement === box){
+        item = original
+    }
+
+    const item = original.cloneNode(true);
+    item.id = "copy_" + (++copyFlower);
+    item.addEventListener("dblclick", () => item.removeChild());
+    box.appendChild(item);
+
+    item.style.position = "absolute";
+    item.style.left = (ev.clientX - rect.left - item.width /2 ) + "px";
+    item.style.top = (ev.clientY - rect.top - item.height /2) + "px";
 }
